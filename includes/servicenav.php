7d72a120c6092671ee9f1c8fd033c5d47a5d3e57
MonishPrasanna <vd.monish@gmail.com>
2026-02-02 03:58:16 +0530

    added nav for services detail pages

--- includes/servicenav.php
+++ includes/servicenav.php
@@ -6,7 +6,7 @@
                                  class="flex justify-between items-center rounded font-semibold
                                  <?php echo in_array($current_page, $managed_pages) ? 'text-orange-700' : 'text-gray-600'; ?>">
                                 Managed Services
-                                <i class="bi bi-chevron-right"></i>
+                                <span class="bg-blue-600 text-white rounded-full w-4 h-4 p-3 flex items-center justify-center"><i class="bi bi-chevron-right"></i></span>
                               </a>
                         
                               <div class="pl-4">
@@ -14,14 +14,14 @@
                                      class="flex justify-between items-center rounded mt-3
                                      <?php echo $current_page == 'accounting_outsourcing.php' ? 'text-orange-700 font-semibold' : 'text-gray-600 hover:bg-gray-100'; ?>">
                                     Accounting Outsourcing
-                                    <i class="bi bi-chevron-right"></i>
+                                    
                                   </a>
                         
                                   <a href="payroll_outsourcing.php"
                                      class="flex justify-between items-center rounded mt-3
                                      <?php echo $current_page == 'payroll_outsourcing.php' ? 'text-orange-700 font-semibold' : 'text-gray-600 hover:bg-gray-100'; ?>">
                                     Payroll Outsourcing
-                                    <i class="bi bi-chevron-right"></i>
+                                    
                                   </a>
                               </div>
                             </div>
@@ -31,28 +31,28 @@
                                  class="flex justify-between items-center rounded font-semibold
                                  <?php echo in_array($current_page, $advisory_pages) ? 'text-orange-700' : 'text-gray-600'; ?>">
                                 Advisory / Consulting
-                                <i class="bi bi-chevron-right"></i>
+                                <span class="bg-blue-600 text-white rounded-full w-4 h-4 p-3 flex items-center justify-center"><i class="bi bi-chevron-right"></i></span>
                               </a>
                                 <div class="pl-4">
                                   <a href="financial_reporting.php"
                                      class="flex justify-between items-center rounded mt-3
                                      <?php echo $current_page == 'financial_reporting.php' ? 'text-orange-700 font-semibold' : 'text-gray-600 hover:bg-gray-100'; ?>">
                                     Financial Reporting
-                                    <i class="bi bi-chevron-right"></i>
+                                    
                                   </a>
                         
                                   <a href="virtual_cfo.php"
                                      class="flex justify-between items-center rounded mt-3
                                      <?php echo $current_page == 'virtual_cfo.php' ? 'text-orange-700 font-semibold' : 'text-gray-600 hover:bg-gray-100'; ?>">
                                     Virtual CFO
-                                    <i class="bi bi-chevron-right"></i>
+                                    
                                   </a>
                         
                                   <a href="business_consulting.php"
                                      class="flex justify-between items-center rounded mt-3
                                      <?php echo $current_page == 'business_consulting.php' ? 'text-orange-700 font-semibold' : 'text-gray-600 hover:bg-gray-100'; ?>">
                                     Business Consulting
-                                    <i class="bi bi-chevron-right"></i>
+                                    
                                   </a>
                               </div>
                             </div>
@@ -62,13 +62,13 @@
                                  class="flex justify-between items-center rounded font-semibold
                                  <?php echo in_array($current_page, $automation_pages) ? 'text-orange-700' : 'text-gray-600'; ?>">
                                 Automation
-                                <i class="bi bi-chevron-right"></i>
+                                <span class="bg-blue-600 text-white rounded-full w-4 h-4 p-3 flex items-center justify-center"><i class="bi bi-chevron-right"></i></span>
                               </a>
                               <div class="pl-4">
-                                  <a href="erp.php" class="flex justify-between items-center rounded mt-3 <?php echo $current_page == 'erp.php' ? 'text-orange-700 font-semibold' : 'text-gray-600 hover:bg-gray-100'; ?>">ERP <i class="bi bi-chevron-right"></i></a>
-                                  <a href="hrms.php" class="flex justify-between items-center rounded mt-3 <?php echo $current_page == 'hrms.php' ? 'text-orange-700 font-semibold' : 'text-gray-600 hover:bg-gray-100'; ?>">HRMS <i class="bi bi-chevron-right"></i></a>
-                                  <a href="crm.php" class="flex justify-between items-center rounded mt-3 <?php echo $current_page == 'crm.php' ? 'text-orange-700 font-semibold' : 'text-gray-600 hover:bg-gray-100'; ?>">CRM <i class="bi bi-chevron-right"></i></a>
-                                  <a href="yapp.php" class="flex justify-between items-center rounded mt-3 <?php echo $current_page == 'yapp.php' ? 'text-orange-700 font-semibold' : 'text-gray-600 hover:bg-gray-100'; ?>">YAPP <i class="bi bi-chevron-right"></i></a>
+                                  <a href="erp.php" class="flex justify-between items-center rounded mt-3 <?php echo $current_page == 'erp.php' ? 'text-orange-700 font-semibold' : 'text-gray-600 hover:bg-gray-100'; ?>">ERP</i></a>
+                                  <a href="hrms.php" class="flex justify-between items-center rounded mt-3 <?php echo $current_page == 'hrms.php' ? 'text-orange-700 font-semibold' : 'text-gray-600 hover:bg-gray-100'; ?>">HRMS</i></a>
+                                  <a href="crm.php" class="flex justify-between items-center rounded mt-3 <?php echo $current_page == 'crm.php' ? 'text-orange-700 font-semibold' : 'text-gray-600 hover:bg-gray-100'; ?>">CRM</i></a>
+                                  <a href="yapp.php" class="flex justify-between items-center rounded mt-3 <?php echo $current_page == 'yapp.php' ? 'text-orange-700 font-semibold' : 'text-gray-600 hover:bg-gray-100'; ?>">YAPP</i></a>
                               </div>
                             </div>
 
@@ -76,29 +76,29 @@
                               <a href="tax.php"
                                  class="flex justify-between items-center rounded font-semibold
                                  <?php echo in_array($current_page, $tax_pages) ? 'text-orange-700' : 'text-gray-600'; ?>">
-                                Tax (Tax Compliance and Tax Planning)
-                                <i class="bi bi-chevron-right"></i>
+                                  Tax Compliance and Tax Planning
+                                <span class="bg-blue-600 text-white rounded-full w-4 h-4 p-3 flex items-center justify-center"><i class="bi bi-chevron-right"></i></span>
                               </a>
                               <div class="pl-4">
                                   <a href="tax_compliance.php"
                                      class="flex justify-between items-center rounded mt-3
                                      <?php echo $current_page == 'tax_compliance.php' ? 'text-orange-700 font-semibold' : 'text-gray-600 hover:bg-gray-100'; ?>">
                                     Tax Compliance
-                                    <i class="bi bi-chevron-right"></i>
+                                    
                                   </a>
                         
                                   <a href="transaction_advisory.php"
                                      class="flex justify-between items-center rounded mt-3
                                      <?php echo $current_page == 'transaction_advisory.php' ? 'text-orange-700 font-semibold' : 'text-gray-600 hover:bg-gray-100'; ?>">
                                     Transaction Advisory
-                                    <i class="bi bi-chevron-right"></i>
+                                    
                                   </a>
                         
                                   <a href="digitsation.php"
                                      class="flex justify-between items-center rounded mt-3
                                      <?php echo $current_page == 'digitsation.php' ? 'text-orange-700 font-semibold' : 'text-gray-600 hover:bg-gray-100'; ?>">
                                     Digitisation
-                                    <i class="bi bi-chevron-right"></i>
+                                    
                                   </a>
                               </div>
                             </div>
